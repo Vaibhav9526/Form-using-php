c83dc8197ef5792e77e9c272b1cd5b814e4ede31
Vaibhav9526 <81508584+Vaibhav9526@users.noreply.github.com>
2022-03-18 15:10:34 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <form action="data.php" method="post">
+    <div class="parent">
+    <div class="container">
+    <h2>This is a project about returning name and email Id using PHP</h2>
+    <br><br><br>
+    <input type="text" id="input1" name="Name" placeholder="enter your name">
+    <br><br><br>
+    <input type="text" id="input2" name="Email" placeholder="enter your Email"> 
+    <br><br><br>
+    <input type="submit" id="input3" name="Enter" value="Submit">
+    <br><br><br><br>
+    </div>
+    </div>
+    </form>
+</body>
+</html>
